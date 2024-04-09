6441b55246961793d13bf15946a4ff9bc0660c78
nhatlinh <nhatlinhdev201@gmail.com>
2024-04-10 02:02:05 +0700

    update schema

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,5 @@ const authController = require("../controllers/auth");
 
 router.route("/auth/register").post(authController.register);
 router.route("/auth/login").post(authController.login);
-/* router.route("/auth/change-password").post(authController.changePassword) */
 
 module.exports = router;
